b1e6a809081f32454bd3dc0309c6d7e42dad0f2c
ursus164 <Galawieprzowa@gmail.com>
2024-10-29 22:16:37 +0100

    Reverted changes, and added paper clip for attachements

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,8 @@
 import Header from "./components/Header";
 import ChatContextProvider from "./store/chat-context.jsx";
-// import Input from "./components/Input.jsx";
+import Input from "./components/Input.jsx";
 import { useEffect, useState } from "react";
 import Loader from "./components/Loader.jsx";
-import "@chatscope/chat-ui-kit-styles/dist/default/styles.min.css";
-import {
-  ChatContainer,
-  MainContainer,
-  MessageList,
-  Message,
-  MessageInput,
-  TypingIndicator,
-} from "@chatscope/chat-ui-kit-react";
 
 function App() {
   const [isLoading, setIsLoading] = useState(true);
@@ -33,25 +24,7 @@ function App() {
       ) : (
         <>
           <Header />
-          <div className="container">
-            <MainContainer>
-              <ChatContainer>
-                <MessageList>
-                  <Message
-                    model={{
-                      message: "Hello, how can I help you?",
-                      sentTime: "Just now",
-                      sender: "bot",
-                    }}
-                  />
-                </MessageList>
-                <MessageInput placeholder="Type a message..." />
-                <TypingIndicator content="bot is typing..." />
-              </ChatContainer>
-            </MainContainer>
-          </div>
-
-          {/* <Input /> */}
+          <Input />
         </>
       )}
     </ChatContextProvider>

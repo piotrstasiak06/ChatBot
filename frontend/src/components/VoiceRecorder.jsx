769d1fa3995f3333cab238f0da3c7d7597616ab8
ursus164 <Galawieprzowa@gmail.com>
2024-10-30 20:49:47 +0100

    Added gif animation for header logo - it looks better now

---
+++ frontend/src/components/VoiceRecorder.jsx
@@ -0,0 +1,72 @@
+import { useRef} from "react";
+import microphone from "../assets/icons8-mic-80.png";
+import stop from "../assets/32px-Solid_white.svg.png";
+import { useContext } from "react";
+import { ChatContext } from "../store/chat-context";
+import axios from "axios";
+
+const VoiceRecorder = () => {
+  const { addMessage } = useContext(ChatContext);
+
+//   const [recordedUrl, setRecordedUrl] = useState("");
+  const mediaStream = useRef(null);
+  const mediaRecorder = useRef(null);
+  const chunks = useRef([]);
+  const startRecording = async () => {
+    try {
+      const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+      mediaStream.current = stream;
+      mediaRecorder.current = new MediaRecorder(stream);
+      mediaRecorder.current.ondataavailable = (e) => {
+        if (e.data.size > 0) {
+          chunks.current.push(e.data);
+        }
+      };
+      mediaRecorder.current.onstop = () => {
+        const recordedBlob = new Blob(chunks.current, { type: "audio/webm" });
+        const url = URL.createObjectURL(recordedBlob);
+        // setRecordedUrl(url);
+        addMessage({ message: url, type: "audio" });
+        uploadRecording(recordedBlob);
+        chunks.current = [];
+      };
+      mediaRecorder.current.start();
+    } catch (error) {
+      console.error("Error accessing microphone:", error);
+    }
+  };
+  const stopRecording = () => {
+    if (mediaRecorder.current && mediaRecorder.current.state === "recording") {
+      mediaRecorder.current.stop();
+    }
+    if (mediaStream.current) {
+      mediaStream.current.getTracks().forEach((track) => {
+        track.stop();
+      });
+    }
+  };
+
+  const uploadRecording = (blob) => {
+    console.log("Uploading recording...");
+    let data = new FormData();
+    data.append('text', "this is the transcription of the audio file");
+    data.append('wavfile', blob, "recording.wav");
+
+    const config = {
+      headers: { 'content-type': 'multipart/form-data' }
+    };
+    axios.post('http://localhost:8080/asr/', data, config);
+  };
+  return (
+    <>
+      {/* <audio controls src={recordedUrl} /> */}
+      <button onClick={startRecording} className="voice">
+        <img src={microphone} alt="start recording" className="voice-img" />
+      </button>
+      <button onClick={stopRecording} className="voice">
+        <img src={stop} className="voice-stop" alt="stop recording" />
+      </button>
+    </>
+  );
+};
+export default VoiceRecorder;
